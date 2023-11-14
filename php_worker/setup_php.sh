1813566b7c81244b337b5a5e435a2bdff2d90be0
Caknoooo <caknocomel@gmail.com>
2023-11-14 16:31:29 +0700

    feat: setup php worker

---
+++ php_worker/setup_php.sh
@@ -0,0 +1,40 @@
+echo 'nameserver 192.173.1.2' > /etc/resolv.conf
+apt-get update
+apt-get install nginx -y
+apt-get install wget -y
+apt-get install unzip -y
+apt-get install lynx -y
+apt-get install php7.3-fpm php7.3-common php7.3-mysql php7.3-gmp php7.3-curl php7.3-intl php7.3-mbstring php7.3-xmlrpc php7.3-gd php7.3-xml php7.3-cli php7.3-zip -y
+
+service nginx start
+service php7.3-fpm start
+
+wget -O '/var/www/granz.channel.a09.com' 'https://drive.google.com/u/0/uc?id=1ViSkRq7SmwZgdK64eRbr5Fm1EGCTPrU1&export=download'
+unzip -o /var/www/granz.channel.a09.com -d /var/www/
+rm /var/www/granz.channel.a09.com
+mv /var/www/modul-3 /var/www/granz.channel.a09.com
+
+cp /etc/nginx/sites-available/default /etc/nginx/sites-available/granz.channel.a09.com
+ln -s /etc/nginx/sites-available/granz.channel.a09.com /etc/nginx/sites-enabled/
+rm /etc/nginx/sites-enabled/default
+
+echo 'server {
+    listen 80;
+    server_name granz.channel.a09.com www.granz.channel.a09.com;
+
+    root /var/www/granz.channel.a09.com;
+    index index.php index.html index.htm;
+
+    location / {
+        try_files $uri $uri/ /index.php?$query_string;
+    }
+
+    location ~ \.php$ {
+        include snippets/fastcgi-php.conf;
+        fastcgi_pass unix:/run/php/php7.3-fpm.sock;  # Sesuaikan versi PHP dan socket
+        fastcgi_param SCRIPT_FILENAME $document_root$fastcgi_script_name;
+        include fastcgi_params;
+    }
+}' > /etc/nginx/sites-available/granz.channel.a09.com
+
+service nginx restart
--- setup/setup_dhcp.sh
+++ setup/setup_dhcp.sh
@@ -2,8 +2,6 @@ echo 'nameserver 192.173.1.2' > /etc/resolv.conf
 apt-get update
 apt install isc-dhcp-server -y
 
-service isc-dhcp-server start
-
 # 2
 # Client yang melalui Switch3 mendapatkan range IP dari [prefix IP].3.16 - [prefix IP].3.32 dan [prefix IP].3.64 - [prefix IP].3.80 (2)
 echo 'subnet 192.173.1.0 netmask 255.255.255.0 {
@@ -88,4 +86,54 @@ subnet 192.173.4.0 netmask 255.255.255.0 {
     option domain-name-servers 192.173.1.2;
     default-lease-time 720;
     max-lease-time 5760;
+}
+
+host Lawine {
+    hardware ethernet 8e:c1:16:c2:6d:4b;
+    fixed-address 192.173.3.3;
+}
+
+host Linie {
+    hardware ethernet 7a:e7:1c:d2:ae:44;
+    fixed-address 192.173.3.2;
+}
+
+host Lunger {
+    hardware ethernet 8a:02:fd:bb:05:be;
+    fixed-address 192.173.3.1;
+}
+
+host Frieren {
+    hardware ethernet 9e:d5:2c:df:39:32;
+    fixed-address 192.173.4.3;
+}
+
+host Flamme {
+    hardware ethernet da:cc:d9:29:59:e8;
+    fixed-address 192.173.4.2;
+}
+
+host Fern {
+    hardware ethernet 8a:31:c6:e6:a2:7b;
+    fixed-address 192.173.4.1;
 }' > /etc/dhcp/dhcpd.conf
+
+echo '# Defaults for isc-dhcp-server (sourced by /etc/init.d/isc-dhcp-server)
+
+# Path to dhcpd's config file (default: /etc/dhcp/dhcpd.conf).
+#DHCPDv4_CONF=/etc/dhcp/dhcpd.conf
+#DHCPDv6_CONF=/etc/dhcp/dhcpd6.conf
+
+# Path to dhcpd's PID file (default: /var/run/dhcpd.pid).
+#DHCPDv4_PID=/var/run/dhcpd.pid
+#DHCPDv6_PID=/var/run/dhcpd6.pid
+
+# Additional options to start dhcpd with.
+#       Don't use options -cf or -pf here; use DHCPD_CONF/ DHCPD_PID instead
+#OPTIONS=""
+
+# On what interfaces should the DHCP server (dhcpd) serve DHCP requests?
+#       Separate multiple interfaces with spaces, e.g. "eth0 eth1".
+INTERFACESv4="eth0"' > /etc/default/isc-dhcp-server
+
+service isc-dhcp-server start
